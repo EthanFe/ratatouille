c468fb4944cd07842db533bc257acfa59f713b7d
Nick Luong <nckluong@gmail.com>
2018-10-03 15:57:47 -0500

    made a lot of pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  resources :chefs, only: [:new, :create]
   resources :rounds, only: [:new, :show, :create]
   get 'rounds/:id/orders', to:'rounds#orders'
   get 'rounds/:id/result', to:'rounds#result'
   post 'rounds/:id/order_finished', to:'rounds#order_finished'
   get 'chefs/login', to: 'chefs#login_page', as: "chefs_login_page"
+  get 'chefs/leaderboard', to: 'chefs#leaderboard', as: "leaderboard"
   post 'chefs/login'
   post 'chefs/logout'
+  resources :chefs, only: [:new, :create,:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
